b14878de1fdcda39208a2716b765f58b37c82ca4
Harold <haroldrex12331@gmail.com>
2023-12-17 21:46:35 +0800

    blog 3

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -18,18 +18,18 @@ const BlogList = () => {
             }}
           >
             <h1>Latest Blog</h1>
-            <BlogCards type="primary" blogId={3} />
+            <BlogCards type="primary" blogId={4} />
           </div>
           <div className="flex justify-center h-[25%]">
             <div className="flex justify-between w-[90%]">
+              <div>
+                <BlogCards type="secondary" blogId={3} />
+              </div>
               <div>
                 <BlogCards type="secondary" blogId={2} />
               </div>
               <div>
                 <BlogCards type="secondary" blogId={1} />
-              </div>
-              <div>
-                <BlogCards type="secondary" blogId={0} />
               </div>
             </div>
           </div>
